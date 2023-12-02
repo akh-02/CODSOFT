ca4eff8b414b0ea9a007959e7aa042c3a85e1438
akh-02 <152690418+akh-02@users.noreply.github.com>
2023-12-02 16:58:48 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,28 @@
+def add(x, y):
+    return x + y
+def sub(x, y):
+    return x - y
+def mul(x, y):
+    return x * y
+def div(x, y):
+    return x / y
+def rem(x, y):
+    return x % y
+
+print("""SELECT THE OPERATION:\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Divide-Remainder""")
+num1=int(input("Please enter the first number:"))
+num2=int(input("Please enter the second number:"))
+
+choice = int(input("Enter choice - 1,2,3,4,5: "))
+if choice == 1:
+    print(num1, "+", num2, "=", add(num1, num2))
+elif choice == 2:
+    print(num1, "-", num2, "=", sub(num1, num2))
+elif choice == 3:
+    print(num1, "*", num2, "=", mul(num1, num2))
+elif choice == 4:
+    print(num1, "/", num2, "=", div(num1, num2))
+elif choice == 5:
+    print(num1, "%", num2, "=", rem(num1, num2))
+else:
+    print("INVALID CHOICE!!")
---
+++ rock_paper_scissors.py
@@ -0,0 +1,26 @@
+import random
+print('The rules of the game ROCK PAPER SCISSORS are :\n'
+      + "Rock vs Paper -> Paper covers rock; Paper wins \n"
+      + "Rock vs Scissors -> Rock smashes scissors; Rock wins \n"
+      + "Paper vs Scissors -> Scissors cut paper; Scissor wins \n")
+user = input("Enter a choice - rock, paper or scissors: ")
+possible_actions = ["rock", "paper", "scissors"]
+computer = random.choice(possible_actions)
+print(f"\nYou chose {user}, computer chose {computer}\n")
+if user == computer:
+    print(f"Both players selected {user}. It's a tie!")
+elif user == "rock":
+    if computer == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose :(")
+elif user == "paper":
+    if computer == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose :(")
+elif user == "scissors":
+    if computer == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose :(")
