b5c507a3e251f285261e27656f4211a0832fbdb1
Akhila <152690418+akh-02@users.noreply.github.com>
2023-12-04 21:44:36 +0530

    rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,12 +1,25 @@
+#importing the random module
 import random
+
+#\u0332 is used to underline the contents that precedes it
+print()
+print("\u0332".join("ROCK VS PAPER VS SCISSOR"))
+print()
+
+# user guide to rules of game
 print('The rules of the game ROCK PAPER SCISSORS are :\n'
       + "Rock vs Paper -> Paper covers rock; Paper wins \n"
       + "Rock vs Scissors -> Rock smashes scissors; Rock wins \n"
       + "Paper vs Scissors -> Scissors cut paper; Scissor wins \n")
+print()
+
+# taking user input as the preferred action
 user = input("Enter a choice - rock, paper or scissors: ")
 possible_actions = ["rock", "paper", "scissors"]
 computer = random.choice(possible_actions)
 print(f"\nYou chose {user}, computer chose {computer}\n")
+
+# comparing the actions and displaying the result
 if user == computer:
     print(f"Both players selected {user}. It's a tie!")
 elif user == "rock":
